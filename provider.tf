4a88fac96c87877efc237fe9c1b87fb120a8947f
root <root@ter-debian.domain.local>
2023-07-07 17:59:07 +0200

    config provider

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+provider "aws" {
+  access_key = "AKIA5D4OXE7V2O2VY26W"
+  secret_key = "UVHwQEFBOed/glzDMSy74BNR0pH+qUHIb9NGbM+s"
+  region     = "eu-west-3"
+  assume_role {
+    role_arn     = "arn:aws:iam::901705574379:role/labalternow-moream"  # Replace with your role ARN
+    source_account_id = "901705574379"              # Add your source account ID
+  }
+}
+
+
 terraform {
   backend "s3" {}
 }
