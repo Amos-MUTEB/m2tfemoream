c69dceb2cb256ddca5034e8f21481cb4fa70fa01
root <root@ter-debian.domain.local>
2023-07-08 04:56:39 +0200

    modify AWS REGION

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIA5D4OXE7V2O2VY26W"
-  secret_key = "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"
+  access_key = "AKIA5D4OXE7VQH7QLRFT"
+  secret_key = "/1lS9HkLzkiyqVrccBYpWbSBJ3gFgESrM//C7Usa"
   region     = "eu-west-3"
   assume_role {
     role_arn     = "arn:aws:iam::901705574379:role/labalternow-moream"  # Replace with your role ARN
