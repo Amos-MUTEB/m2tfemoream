9d3525ed7da10b43ce1e70b15f4d75d29136dc21
Amos-MUTEB <amos.ilungakilumbuamuteb@ynov.com>
2023-07-12 19:26:28 +0200

    adding provider infos

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+provider "aws" {
+  access_key = "AKIAQEGAC45OJ2NUTJ53"
+  secret_key = "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"
+  region     = "eu-west-3"
+  assume_role {
+    role_arn     = "arn:aws:iam::008992778076:role/moream_role"  # Repla>
+    source_account_id = "008992778076"              # Add your source account ID
+  }
+}
 
 terraform {
   backend "s3" {}
